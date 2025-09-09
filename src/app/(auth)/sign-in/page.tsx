13b34445dc82d8d03363d370bb9eac4558f82cbe
Husni <husniwfayo@gmail.com>
2025-09-09 11:45:25 +0530

    centered sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="w-full">
+    <div className="flex mx-auto min-h-screen justify-center items-center">
       <SignIn />
     </div>
   );
